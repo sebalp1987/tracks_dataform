f69956b24b1ff6e31acaf0bc06848960b13159f9
SebaLaptop <sebalp_1987@hotmail.com>
2026-01-08 13:19:49 +0100

    dataform init

---
+++ includes/constants.js
@@ -0,0 +1,6 @@
+module.exports = {
+    RAW_DATASET: dataform.projectConfig.vars.raw_dataset,
+    ANALYTICS_DATASET: dataform.projectConfig.vars.analytics_dataset,
+    MART_DATASET: dataform.projectConfig.vars.mart_dataset,
+    CLIENT_ID: dataform.projectConfig.vars.client_id
+  };
